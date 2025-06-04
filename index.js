99885f99f8c4e542bd36fee1e7109fe8ffaf798c
abu2cap <f.zelmer@web.de>
2025-06-04 16:30:28 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    VANTA.NET({
+        el: "#VantaHintergrund",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: true,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.3,
+        scaleMobile: 1.0,
+        backgroundColor: 0x000000,
+        points: 10.0,
+        maxDistance: 20.0,
+        spacing: 18.0
+    });
+});
